d5f15ecd2fccf65fb4d09ea1e618244b5c2a5b28
BoburXO <boburhatamov7@gmail.com>
2022-11-18 14:43:05 +0500

    header created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "../Header/Header.css";
+import logo from '../../assets/images/logo.svg';
+import clock from '../../assets/images/clock.svg';
+import location from '../../assets/images/location.svg';
+import call from '../../assets/images/call.svg';
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <nav className="header__nav1">
+            <div className="header__container">
+
+            </div>
+        </nav>
+        <nav className="header__nav2">
+            <div className="header__container">
+                
+            </div>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Header;
