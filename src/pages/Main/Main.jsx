091954c9f9fbae33ef90e817e6412aba004ed4cb
BoburXO <boburhatamov7@gmail.com>
2022-11-25 15:13:56 +0500

    maintexts component created

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -3,12 +3,14 @@ import Footer from '../../components/footer/Footer'
 import Header from '../../components/Header/Header'
 import Iframe from '../../components/Iframe/Iframe'
 import MainAside from '../../components/mainAside/MainAside'
+import MainTexts from '../../components/mainTexts/MainTexts'
 
 const Main = () => {
   return (
     <>
     <Header/>
 
+    <MainTexts/>
     <MainAside/>
     <Iframe/>
     <Footer/>
