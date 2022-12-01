524d0de8349910dea510ca5cc4e7b9746e5b2a01
Bobur Hatamov <boburhatamov7@gmail.com>
2022-12-01 11:14:54 +0500

    zagolovok finished

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Clients from '../../components/AboutClients/Clients'
 import EuroKran from '../../components/aboutEuroKran/EuroKran'
 import Facts from '../../components/aboutFacts/Facts'
+import Zagolovok from '../../components/aboutZagolovok/Zagolovok'
 import Footer from '../../components/footer/Footer'
 import Header from '../../components/Header/Header'
 import Iframe from '../../components/Iframe/Iframe'
@@ -15,6 +16,7 @@ const About = () => {
        <Uslugi/>
        <Clients/>
        <Facts/>
+       <Zagolovok/>
 
        <Iframe/>
        <Footer/>
