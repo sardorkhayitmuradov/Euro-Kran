43016a80d8876e054d6fe38971c9aaaee75c3231
Bobur Hatamov <boburhatamov7@gmail.com>
2022-12-07 15:09:01 +0500

    media main page chala

--- src/components/mainCatalog/CatalogTechniques.jsx
+++ src/components/mainCatalog/CatalogTechniques.jsx
@@ -24,7 +24,7 @@ const CatalogTechniques = () => {
         },
       },
       {
-        breakpoint: 600,
+        breakpoint: 768,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
@@ -32,7 +32,7 @@ const CatalogTechniques = () => {
         },
       },
       {
-        breakpoint: 480,
+        breakpoint: 656,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
